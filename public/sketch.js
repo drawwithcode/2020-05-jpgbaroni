a75145b3e0314c95cda8819a75829d06cfd155ef
jpgbaroni <jeanpaul.baroni@mail.polimi.it>
2020-11-24 00:58:27 +0100

    welcome

--- public/sketch.js
+++ public/sketch.js
@@ -1,5 +1,8 @@
 // Create a new connection using socket.io (imported in index.html)
 let socket = io();
+let fps = 20;
+let welcomeMessage = ["imagine that","there was no lockdown","and you could travel","travel everywhere","where would you go?"]
+let secondsPerMessage = 1.5;
 
 // define the function that will be called on a new newConnection
 socket.on("connect", newConnection);
@@ -21,6 +24,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
   textFont("texturina");
+  frameRate(fps);
 }
 
 function windowResized() {
@@ -57,5 +61,21 @@ function mouseDragged() {
 function draw() {
   // evert draw cycle, add a background with low opacity
   // to create the "fade" effect
-  background(0, 5);
+  background(0);
+
+  if (frameCount/fps < welcomeMessage.length()*secondsPerMessage+1) {
+    push();
+    noStroke();
+    textAlign(CENTER,CENTER);
+    for (let i = 0; i < sentence.length(); i++) {
+      fill(255);
+      if(frameCount/fps/secondsPerMessage > i) {
+        fill(255,255,255,max(0,-frameCount/fps/secondsPerMessage+1+i)*255);
+      }
+      textSize(frameCount/fps*secondsPerMessage/i*windowHeight/2);
+      text(sentence[i], windowWidth/2, windowHeight/2);
+    }
+    pop();
+  }
+
 }
